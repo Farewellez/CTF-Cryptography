1e8752115b53cec2e8ba6aeabadb7d687e2b8483
kurumi- <ahmadzafarell55@gmail.com>
2026-07-14 22:08:55 +0700

    Add files via upload

---
+++ 1.cryptopals/set_1/write-up/chall_1/solve.py
@@ -0,0 +1,6 @@
+# Convert hex to base64
+import base64
+
+pt = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+ct = base64.b64encode(bytes.fromhex(pt)).decode()
+print(ct)
